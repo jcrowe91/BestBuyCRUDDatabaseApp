035475ec4c4aba47e1cc2ad8da1b5d999fcec4f8
jcrowe91 <jcrowe9119@gmail.com>
2022-02-01 17:12:31 -0600

    Add project files.

---
+++ BestBuyCRUDDatabaseConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BestBuyCRUDDatabaseConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
